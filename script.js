a98e2fc7147e3123ad4a797e93d9fa335eeda481
Valentin <valentin.segalla@etu.univ-lyon1.fr>
2022-11-19 16:59:26 +0100

    remaniment du script : mise en place des classes, structure sous forme de tableau

--- script.js
+++ script.js
@@ -1,56 +1,109 @@
 const plateau = document.getElementById("snakeGame");
 const context = snakeGame.getContext("2d");
 
-const canvaHeight = plateau.height;
-const canvaWidth = plateau.width;
-const tailleBlock = canvaHeight/5;
-
 var monde =[
-    [[0,0],[0,1],[0,2],[0,3],[0,4],[0,5]],
-    [[1,0],[1,1],[1,2],[1,3],[1,4],[1,5]],
-    [[2,0],[2,1],[2,2],[2,3],[2,4],[2,5]],
-    [[3,0],[3,1],[3,2],[3,3],[3,4],[3,5]],
-    [[4,0],[4,1],[4,2],[4,3],[4,4],[4,5]],
-    [[5,0],[5,1],[5,2],[5,3],[5,4],[5,5]],
+    [[0,0],[0,1],[0,2],[0,3],[0,4],[0,5],[0,6],[0,7],[0,8],[0,9]],
+    [[1,0],[1,1],[1,2],[1,3],[1,4],[1,5],[1,6],[1,7],[1,8],[1,9]],
+    [[2,0],[2,1],[2,2],[2,3],[2,4],[2,5],[2,6],[2,7],[2,8],[2,9]],
+    [[3,0],[3,1],[3,2],[3,3],[3,4],[3,5],[3,6],[3,7],[3,8],[3,9]],
+    [[4,0],[4,1],[4,2],[4,3],[4,4],[4,5],[4,6],[4,7],[4,8],[4,9]],
+    [[5,0],[5,1],[5,2],[5,3],[5,4],[5,5],[5,6],[5,7],[5,8],[5,9]],
+    [[6,0],[6,1],[6,2],[6,3],[6,4],[6,5],[6,6],[6,7],[6,8],[6,9]],
+    [[7,0],[7,1],[7,2],[7,3],[7,4],[7,5],[7,6],[7,7],[7,8],[7,9]],
+    [[8,0],[8,1],[8,2],[8,3],[8,4],[8,5],[8,6],[8,7],[8,8],[8,9]],
+    [[9,0],[9,1],[9,2],[9,3],[9,4],[9,5],[9,6],[9,7],[9,8],[9,9]],
 ];
 
-//liste correspondant au corps du snake
-var snake = [
-    [0,2]
-]
+const canvaHeight = plateau.height;
+const canvaWidth = plateau.width;
+const longueurBlock = canvaWidth/monde.length;
+const largeurBlock = canvaHeight/monde.length;
+
+
+
+//classe correspondant au snake
+class Snake {
+    constructor() {
+        this.corps = [[0,2]];
+    }
+    //fonction pour dessiner le serpent
+    dessinerSnake(){
+        this.corps.forEach(element=>dessinerCorps(element));
+    }
+    deplacer(direction){
+        switch(direction){
+            case 'haut':
+                effacer(this.corps[this.corps.length-1]);
+                this.corps.forEach(element=>{
+                    console.log(element);
+                    element[1]-=1;
+                });
+                break;
+            case 'bas':
+                effacer(this.corps[this.corps.length-1]);
+                this.corps.forEach(element=>{
+                    element[1]+=1;
+                });
+                break;
+            case 'gauche':
+                effacer(this.corps[this.corps.length-1]);
+                this.corps.forEach(element=>{
+                    element[0]-=1;
+                });
+                break;
+            case 'droite':
+                effacer(this.corps[this.corps.length-1]);
+                this.corps.forEach(element=>{
+                    element[0]+=1;
+                });
+                break;
+        }
+        this.dessinerSnake();
+    }
+    //fonction pour effacer le snake lors d'une nouvelle partie
+    effacerSnake(){
+        this.corps.forEach(element=>effacer(element));
+        this.corps = [[3,3]];
+    }
+    fruitManger(){
+        if(this.corps[0].join()===fruit.position.join())
+            return true;
+        return false;
+    }
+}
+    
 var direction = 'haut';
-var fruit;
+
+//classe représentant le fruit
+class Fruit {
+    
+    constructor(){
+        this.position = [];
+    }
+    
+    dessinerFruit(){
+        context.fillStyle = 'red';
+        let abscisse = entierAleatoire(0,5);
+        let ordonnee = entierAleatoire(0,5);
+        context.fillRect(abscisse*longueurBlock, ordonnee*largeurBlock, longueurBlock, largeurBlock);
+        this.position = [abscisse,ordonnee];
+    }
+};
 
 function dessinerMonde(){
     context.fillStyle = 'green';
-    monde.forEach(cellule=>{
-        context.fillRect(cellule[0]*tailleBlock,cellule[1]*tailleBlock, tailleBlock, tailleBlock);
+    monde.forEach(ligne=>{
+        ligne.forEach(cellule=>{
+             context.fillRect(cellule[0]*longueurBlock,cellule[1]*largeurBlock, longueurBlock, largeurBlock);
+        });
+       
     });
 }
 
 //fonction permettant le dessin d'une partie du corps
-function dessinerCorps(corps){
+function dessinerCorps(position){
     context.fillStyle = 'blue';
-    //context.strokestyle = 'darkblue';
-    context.fillRect(corps[0]*tailleBlock, corps[1]*tailleBlock, tailleBlock, tailleBlock);
-    //context.strokeRect(corps.x, corps.y, 5, 5);
-}
-
-function dessinerfruit(){
-    context.fillStyle = 'red';
-    //context.strokestyle = 'red';
-    let abscisse = entierAleatoire(0,plateau.width-5);
-    let ordonnee = entierAleatoire(0,plateau.height-5);
-
-    context.fillRect(abscisse, ordonnee, 5, 5);
-    //context.strokeRect(abscisse, ordonnee, 5, 5);
-    return {x:abscisse,y:ordonnee}
-}
-
-
-//fonction pour dessiner le serpent
-function dessinerSnake(){
-    snake.forEach(element=>dessinerCorps(element));
+    context.fillRect(position[0]*longueurBlock, position[1]*largeurBlock, longueurBlock, largeurBlock);
 }
 
 document.addEventListener('keydown',(evt)=>{
@@ -70,55 +123,10 @@ document.addEventListener('keydown',(evt)=>{
     }
 });
 
-function deplacer(direction){
-    switch(direction){
-        case 'haut':
-            
-            effacer(snake[snake.length-1]);
-            snake.forEach(element=>{
-                element.y-=5;
-            });
-            dessinerCorps(snake[0]);
-            break;
-        case 'bas':
-            
-            effacer(snake[snake.length-1]);
-            snake.forEach(element=>{
-                element.y+=5;
-            });
-            dessinerCorps(snake[0]);
-            break;
-        case 'gauche':
-            
-            effacer(snake[snake.length-1]);
-            snake.forEach(element=>{
-                element.x-=5;
-            });
-            dessinerCorps(snake[0]);
-            break;
-        case 'droite':
-        
-            effacer(snake[snake.length-1]);
-            snake.forEach(element=>{
-                element.x+=5;
-            });
-            dessinerCorps(snake[0]);
-            break;
-    }
-}
-
 //fonction pour effacer un élément sur le plateau
-function effacer(coordonnee){
+function effacer(position){
     context.fillStyle = 'green';
-    //context.strokestyle = 'green';
-    context.fillRect(coordonnee.x, coordonnee.y, 5, 5);
-    //context.strokeRect(coordonnee.x, coordonnee.y, 5, 5);
-}
-
-//fonction pour effacer le snake lors d'une nouvelle partie
-function effacerSnake(){
-    snake.forEach(element=>effacer(element));
-    snake = [{x:100, y:100}]
+    context.fillRect(position[0]*longueurBlock, position[1]*largeurBlock, longueurBlock, largeurBlock);
 }
 
 //fonction pour obtenir un nombre aléatoire
@@ -127,25 +135,22 @@ function entierAleatoire(min, max)
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function fruitManger(){
-    if(snake[0].x+5 >= fruit.x && snake[0].y+5 >= fruit.y && snake[0].y+5 <= fruit.y+5 && snake[0].x+5 <= fruit.x+5){
-        effacer(fruit);
-        fruit = dessinerfruit();
-    }
-}
-
 function step(){
-    // if(fruit!=undefined||fruit!=null)
-    //     effacer(fruit);
-    dessinerSnake();
-   
-    deplacer(direction);
-    fruitManger();
+    if(snake.fruitManger() || fruit.position.join()===[].join()){
+        effacer(fruit.position);
+        fruit.dessinerFruit();
+    }
+    snake.deplacer(direction);
 
-    setTimeout(step,10);
+    setTimeout(step,500);
 }
+
+var fruit;
+var snake;
 
 (()=>{ 
-    fruit = dessinerfruit();
+    snake = new Snake();
+    fruit = new Fruit();
+    dessinerMonde();
     step();
 })();
