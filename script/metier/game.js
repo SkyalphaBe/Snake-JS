68398abe480969fe0877534e5b77900fcba09cb7
François Graux <93640820+SkyalphaBe@users.noreply.github.com>
2022-12-01 23:36:13 +0100

    bug fixes gameOver

--- script/metier/game.js
+++ script/metier/game.js
@@ -29,7 +29,7 @@ oldScore.createStorage();
 /**
  * Classe contenant toute les méthodes fonctionnelles du jeu
  */
-class Game{
+class Game {
     constructor() {
         this.context = null;
         this.fruit = null;
@@ -40,15 +40,16 @@ class Game{
         this.score = 0;
         this.nbFruit = 0;
     }
+
     /**
      * Méthode pour rendre l'état d'origine à la carte (toutes les cases a 'EMPTY")
      * @param size attribut qui represente la tableau qui compose la carte
      */
-    resetWorld(size){
-        this.world=[];
-        for(let i = 0 ;i<size[0];i++){
+    resetWorld(size) {
+        this.world = [];
+        for (let i = 0; i < size[0]; i++) {
             let subTab = [];
-            for(let j = 0;j<size[1];j++){
+            for (let j = 0; j < size[1]; j++) {
                 subTab.push('EMPTY');
             }
             this.world.push(subTab);
@@ -58,35 +59,32 @@ class Game{
     /**
      * Méthode qui permet de dessiner les objets en fonction de leurs position
      */
-    drawWorld(){
-        for(let i = 0; i<this.world.length;i++){
-            for(let j = 0; j<this.world.length;j++){
-                if(this.world[i][j]=='SNAKE'){
+    drawWorld() {
+        for (let i = 0; i < this.world.length; i++) {
+            for (let j = 0; j < this.world.length; j++) {
+                if (this.world[i][j] == 'SNAKE') {
                     this.context.fillStyle = '#775EFF';
-                    this.context.fillRect(i*this.sizeBlockX,j*this.sizeBlockY, this.sizeBlockX, this.sizeBlockY);
-                    if([i,j].join()==this.snake.bodySnake[0].join()){
+                    this.context.fillRect(i * this.sizeBlockX, j * this.sizeBlockY, this.sizeBlockX, this.sizeBlockY);
+                    if ([i, j].join() == this.snake.bodySnake[0].join()) {
                         this.drawEyes();
                     }
 
-                }
-                else if(this.world[i][j]=='EMPTY'){
-                    if(i%2==0&&j%2==0||i%2!=0&&j%2!=0)
-                        this.context.fillStyle ='#DDD5D0';
+                } else if (this.world[i][j] == 'EMPTY') {
+                    if (i % 2 == 0 && j % 2 == 0 || i % 2 != 0 && j % 2 != 0)
+                        this.context.fillStyle = '#DDD5D0';
                     else
-                        this.context.fillStyle='#B0AFAF';
-                    this.context.fillRect(i*this.sizeBlockX,j*this.sizeBlockY, this.sizeBlockX, this.sizeBlockY);
-                }
-                else if(this.world[i][j]=='WALL' && wallcheckBox.checked){
-                    this.context.fillStyle='Brown';
-                    this.context.fillRect(i*this.sizeBlockX,j*this.sizeBlockY, this.sizeBlockX, this.sizeBlockY);
-                }
-                else if (this.world[i][j]=='FRUIT'){
-                    if(i%2==0&&j%2==0||i%2!=0&&j%2!=0)
-                        this.context.fillStyle ='#DDD5D0';
+                        this.context.fillStyle = '#B0AFAF';
+                    this.context.fillRect(i * this.sizeBlockX, j * this.sizeBlockY, this.sizeBlockX, this.sizeBlockY);
+                } else if (this.world[i][j] == 'WALL' && wallcheckBox.checked) {
+                    this.context.fillStyle = 'Brown';
+                    this.context.fillRect(i * this.sizeBlockX, j * this.sizeBlockY, this.sizeBlockX, this.sizeBlockY);
+                } else if (this.world[i][j] == 'FRUIT') {
+                    if (i % 2 == 0 && j % 2 == 0 || i % 2 != 0 && j % 2 != 0)
+                        this.context.fillStyle = '#DDD5D0';
                     else
-                        this.context.fillStyle='#B0AFAF';
-                    this.context.fillRect(i*this.sizeBlockX,j*this.sizeBlockY, this.sizeBlockX, this.sizeBlockY);
-                    this.context.drawImage(appleCanva,i*this.sizeBlockX, j*this.sizeBlockY, this.sizeBlockX, this.sizeBlockY);
+                        this.context.fillStyle = '#B0AFAF';
+                    this.context.fillRect(i * this.sizeBlockX, j * this.sizeBlockY, this.sizeBlockX, this.sizeBlockY);
+                    this.context.drawImage(appleCanva, i * this.sizeBlockX, j * this.sizeBlockY, this.sizeBlockX, this.sizeBlockY);
                 }
             }
         }
@@ -95,27 +93,26 @@ class Game{
     /**
      * Méthode pour dessinner les yeux sur la tête du serpent
      */
-    drawEyes(){
-        this.context.fillStyle='black';
-        if(this.snake.direction=='haut'||this.snake.direction=='bas'){
+    drawEyes() {
+        this.context.fillStyle = 'black';
+        if (this.snake.direction == 'haut' || this.snake.direction == 'bas') {
             this.context.beginPath();
-            this.context.arc(this.snake.bodySnake[0][0]*this.sizeBlockX+this.sizeBlockX/4,this.snake.bodySnake[0][1]
-                *this.sizeBlockY+this.sizeBlockY/2,this.sizeBlockX/8,0,2*Math.PI);
+            this.context.arc(this.snake.bodySnake[0][0] * this.sizeBlockX + this.sizeBlockX / 4, this.snake.bodySnake[0][1]
+                * this.sizeBlockY + this.sizeBlockY / 2, this.sizeBlockX / 8, 0, 2 * Math.PI);
             this.context.fill();
             this.context.beginPath();
-            this.context.arc(this.snake.bodySnake[0][0]*this.sizeBlockX+this.sizeBlockX/4*3,
-                this.snake.bodySnake[0][1]*this.sizeBlockY+this.sizeBlockY/2,this.sizeBlockX/8,0,2*Math.PI);
+            this.context.arc(this.snake.bodySnake[0][0] * this.sizeBlockX + this.sizeBlockX / 4 * 3,
+                this.snake.bodySnake[0][1] * this.sizeBlockY + this.sizeBlockY / 2, this.sizeBlockX / 8, 0, 2 * Math.PI);
             this.context.fill();
 
-        }
-        else if(this.snake.direction=='gauche'||this.snake.direction=='droite'){
+        } else if (this.snake.direction == 'gauche' || this.snake.direction == 'droite') {
             this.context.beginPath();
-            this.context.arc(this.snake.bodySnake[0][0]*this.sizeBlockX+this.sizeBlockX/2,this.snake.bodySnake[0][1]
-                *this.sizeBlockY +this.sizeBlockY/4,this.sizeBlockX/8,0,2*Math.PI);
+            this.context.arc(this.snake.bodySnake[0][0] * this.sizeBlockX + this.sizeBlockX / 2, this.snake.bodySnake[0][1]
+                * this.sizeBlockY + this.sizeBlockY / 4, this.sizeBlockX / 8, 0, 2 * Math.PI);
             this.context.fill();
             this.context.beginPath();
-            this.context.arc(this.snake.bodySnake[0][0]*this.sizeBlockX+this.sizeBlockX/2,this.snake.bodySnake[0][1]
-                *this.sizeBlockY +this.sizeBlockY/4*3,this.sizeBlockX/8,0,2*Math.PI);
+            this.context.arc(this.snake.bodySnake[0][0] * this.sizeBlockX + this.sizeBlockX / 2, this.snake.bodySnake[0][1]
+                * this.sizeBlockY + this.sizeBlockY / 4 * 3, this.sizeBlockX / 8, 0, 2 * Math.PI);
             this.context.fill();
         }
     }
@@ -124,15 +121,15 @@ class Game{
      * Méthode pour definir la postion des murs
      * @param tabPosition attribut qui represente toutes les positions dans la tableau 'wall' du fichier Json
      */
-    drawWall(tabPosition){
-        for (let i = 0;i<tabPosition.length;i++){
-            this.world[tabPosition[i][0]][tabPosition[i][1]]='WALL';
+    drawWall(tabPosition) {
+        for (let i = 0; i < tabPosition.length; i++) {
+            this.world[tabPosition[i][0]][tabPosition[i][1]] = 'WALL';
         }
     }
 
     //fonction pour effacer le snake lors d'une nouvelle partie
-    deleteSnake(){
-        this.snake.bodySnake.forEach(element=>this.deletePosition(element));
+    deleteSnake() {
+        this.snake.bodySnake.forEach(element => this.deletePosition(element));
     }
 
     /**
@@ -141,18 +138,18 @@ class Game{
      * @param wallList Attribut qui représente ma liste de l'essemble des murs
      * @param time
      */
-    step(speed,wallList,time){
-        if(this.snake.eatFruit(this.fruit) || this.fruit.position.join()===[].join()){
+    step(speed, wallList, time) {
+        if (this.snake.eatFruit(this.fruit) || this.fruit.position.join() === [].join()) {
             this.deletePosition(this.fruit.position);
-            this.fruit.placeFruit(this.world,this.snake,wallList);
+            this.fruit.placeFruit(this.world, this.snake, wallList);
         }
-        if(!this.gameOver(wallList,time)){
+        if (!this.gameOver(wallList, time)) {
             this.deleteSnake();
-            this.snake.moveBody(this.nbFruit,this.score,this.fruit,this.world);
-            this.drawWorld(this.sizeBlockX,this.sizeBlockY);
-            time = setTimeout(()=>{
-                this.step(speed,wallList,this.sizeBlockX,this.sizeBlockY,time);
-            },speed);
+            this.snake.moveBody(this.nbFruit, this.score, this.fruit, this.world);
+            this.drawWorld(this.sizeBlockX, this.sizeBlockY);
+            time = setTimeout(() => {
+                this.step(speed, wallList, this.sizeBlockX, this.sizeBlockY, time);
+            }, speed);
         }
     }
 
@@ -162,85 +159,85 @@ class Game{
      * @param positionSnake Attribut qui représente la position du seprent
      * @param wallList Attribut qui représente la liste des murs
      */
-    start(dimension,positionSnake,wallList){
+    start(dimension, positionSnake, wallList) {
         this.resetWorld(dimension);
-        if (wallcheckBox.checked){
+        if (wallcheckBox.checked) {
             this.drawWall(wallList);
         }
         createHeadFoot();
         createCanvas();
-        menu.style.display="none";
+        menu.style.display = "none";
 
         let plateau = document.getElementById("snakeGame");
         this.context = snakeGame.getContext("2d");
 
-        this.sizeBlockX = plateau.height/this.world.length;
-        this.sizeBlockY = plateau.width/this.world[1].length;
+        this.sizeBlockX = plateau.height / this.world.length;
+        this.sizeBlockY = plateau.width / this.world[1].length;
 
         this.score = document.getElementById("foot").children.item(0);
         this.nbFruit = document.getElementById("fruit").children.item(1);
 
         this.snake = new Snake(positionSnake);
-        this.fruit =new Fruit(dimension);
+        this.fruit = new Fruit(dimension);
     }
 
     //fonction pour effacer un élément sur le plateau
-    deletePosition(position){
-        if(position.join()!=[])
-            this.world[position[0]][position[1]]='EMPTY';
+    deletePosition(position) {
+        if (position.join() != [])
+            this.world[position[0]][position[1]] = 'EMPTY';
     }
 
-    stop(){
+    stop(time) {
         createGameOver();
         this.snake = null;
         clearTimeout(time);
         return true;
     }
+
     /**
      * méthode qui permet de definir quand le joueur perds la partie
      * @param wallList Attribut qui représente la liste des position des mur;
      * @param time valeur su Timeout de la page
-     * @returns {boolean}  renvoie false si pas de condition remplie
+     * @returns {boolean}  renvoie false si pas de condition
      */
     gameOver(wallList,time){
         oldScore.compareNewScore(this.snake.score);
         for(let i = 1; i<this.snake.bodySnake.length;i++){
             if(this.snake.bodySnake[0].join()===this.snake.bodySnake[i].join()){
-                this.stop();
-
+                return this.stop(time);
+            }
+        }
         if(this.snake.bodySnake[0][0]-1==-1&&this.snake.direction=='gauche'){
-            this.stop();
+            return this.stop(time);
         }
         else if(this.snake.bodySnake[0][0]+1==this.world.length&&this.snake.direction=='droite'){
-            this.stop();
+            return this.stop(time);
         }
         else if(this.snake.bodySnake[0][1]-1==-1&&this.snake.direction=='haut'){
-            this.stop();
+            return this.stop(time);
         }
         else if(this.snake.bodySnake[0][1]+1==this.world.length&&this.snake.direction=='bas'){
-            this.stop();
+            return this.stop(time);
         }
         if (wallcheckBox.checked){
             for(let i=0;i<wallList.length;i++){
                 if(this.snake.bodySnake[0][0]+1== wallList[i][0]&&this.snake.bodySnake[0][1]== wallList[i][1]&&this.snake.direction=='droite'){
-                    this.stop();
+                    return this.stop(time);
                 }
                 else if(this.snake.bodySnake[0][0]-1== wallList[i][0]&&this.snake.bodySnake[0][1]== wallList[i][1]&&this.snake.direction=='gauche'){
-                    this.stop();
+                    return this.stop(time);
                 }
                 else if(this.snake.bodySnake[0][0]== wallList[i][0]&&this.snake.bodySnake[0][1]-1== wallList[i][1]&&this.snake.direction=='haut'){
-                    this.stop();
+                    return this.stop(time);
                 }
                 else if (this.snake.bodySnake[0][0]== wallList[i][0]&&this.snake.bodySnake[0][1]+1== wallList[i][1]&&this.snake.direction=='bas'){
-                    this.stop();
+                    return this.stop(time);
                 }
             }
         }
         return false;
     }
 }
-
-
 
 export {Game,wallcheckBox,sizeBoard,oldScore};
 
